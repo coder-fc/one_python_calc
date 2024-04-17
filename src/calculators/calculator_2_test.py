f0a581a12510ffb826e16cbbcedf97ab1f63a802
DevFC-M <felipech3344@gmail.com>
2024-04-17 13:14:12 -0300

    Incluindo o teste unitário

--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -1,6 +1,7 @@
-from typing import Dict
+from typing import Dict, List
 from .calculator_2 import Calculator2
 from src.drivers.numpy_handler import NumpyHandler
+from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
 
 
 class MockRequest:
@@ -8,7 +9,12 @@ class MockRequest:
         self.json = body
 
 
-def test_calculate():
+class MockDriverHandler(DriverHandlerInterface):
+    def standard_derivation(self, numbers: List[float]) -> float:
+        return 3  # Representação do que seria o NumpyHandler (elemento)
+
+# Integração entre NumpyHandler e Calculator2
+def test_calculate_integration():
     mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
 
     driver = NumpyHandler()
@@ -17,3 +23,14 @@ def test_calculate():
 
     assert isinstance(formated_response, dict)
     assert formated_response == {'data': {'Calculator': 2, 'result': 0.08}}
+
+
+def test_calculate():
+    mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
+
+    driver = MockDriverHandler()
+    calculator_2 = Calculator2(driver)
+    formated_response = calculator_2.calculate(mock_request)
+
+    assert isinstance(formated_response, dict)
+    assert formated_response == {'data': {'Calculator': 2, 'result': 0.33}}
