251e838bad67a737e870dbcf066010bbed2e8df2
DevFC-M <felipech3344@gmail.com>
2024-04-16 22:55:24 -0300

    Adionando o conceito de interfaces no projeto da segunda calculadora com metodos abstratos para inclusão de libs externas

--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -1,5 +1,6 @@
 from typing import Dict
 from .calculator_2 import Calculator2
+from src.drivers.numpy_handler import NumpyHandler
 
 
 class MockRequest:
@@ -10,7 +11,8 @@ class MockRequest:
 def test_calculate():
     mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
 
-    calculator_2 = Calculator2()
+    driver = NumpyHandler()
+    calculator_2 = Calculator2(driver)
     formated_response = calculator_2.calculate(mock_request)
 
     assert isinstance(formated_response, dict)
